c42764be0f9bdad820e0a798ad8f54001cff6c0a
Schamir Poliard <schamirpo@gmail.com>
2022-09-04 14:29:43 -0400

    netflix clone update

--- src/Banner.js
+++ src/Banner.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import "./Banner.css"
+import React from 'react';
+import "./Banner.css";
 
 function Banner() {
+
+  function truncate(str, n){
+      return str?.length > n ? str.substring(0, n - 1) + '...' : str;
+  }
+
+
   return (
     <header 
         className= "banner"
         style={{
         backgroundSize: "cover",
-        backgroundImage: `url("https://whatchareading.com/wp-content/uploads/2015/04/Netflix-Marvels-Daredevil-Banner.jpg")`,
+        backgroundImage: `url("https://t3.ftcdn.net/jpg/04/32/27/96/360_F_432279625_PzjAHY8BYDSKkcHR8m0S6n37gFqh68JM.jpg")`,
         backgroundPosition: "center center",
   }}>
 
@@ -20,10 +26,11 @@ function Banner() {
                 <button className="banner__button">My List</button>
             </div>
 
-            <h1 class="banner__description">This is a test description.</h1>
+            <h1 class="banner__description"> {truncate (`Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis voluptatum soluta veniam, ducimus accusantium, cumque deserunt possimus saepe in accusamus itaque suscipit officiis, ratione sint nostrum. Perferendis explicabo laborum, eum incidunt non eligendi commodi corporis? Et dignissimos corporis odit quas?`, 150)}
+              </h1>
         </div>
 
-        <div class="banner--fadeBotton" />
+        <div class="banner--fadeButton" />
 
      
         
